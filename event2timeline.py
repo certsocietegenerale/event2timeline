9026747d6e31dc750935f8825345c11d6df8da0e
Thomas Chopitea <tomchop@gmail.com>
2013-06-28 15:35:24 +0200

    Changed type of data passed to timeline
    Minor fixes
    Comments

--- event2timeline.py
+++ event2timeline.py
@@ -9,7 +9,7 @@
 #                                                                                                      #
 ########################################################################################################
 
-### /!\ You must modify the regexp (s['user'] = re.search) to match the "username" strings in your langage! /!\ ###
+### /!\ You must modify the USERNAME_STRING to match the "username" strings in your langage! /!\ ###
 
 import csv, sys, re
 import optparse
@@ -22,7 +22,7 @@ from Evtx.Evtx import FileHeader
 from Evtx.Views import evtx_file_xml_view
 
 __description__ = "Event2Timeline"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 eid_regex = re.compile('<EventID Qualifiers="(?P<qualifiers>.*)">(?P<eid>\d+)</EventID>')
 sessid_regex = re.compile('<Data Name="TargetLogonId">(?P<session_id>0x[0-9a-fA-F]+)</Data>')
@@ -42,7 +42,7 @@ EVTX_LOGOFF = [
 EVT_LOGIN = [i-4096 for i in EVTX_LOGIN] + [540] # Successful network logon (=4624 in EVTX)
 EVT_LOGOFF = [i-4096 for i in EVTX_LOGOFF]
 
-USERNAME_STRING = 'tilisateur'
+USERNAME_STRING = 'tilisateur' ###### MODIFY THIS TO WHICHEVER LANGUAGE YOUR CSV FILE IS ######
 
 def get_data(xml, name):
 	rex = re.compile('<Data Name="%s">(?P<%s>.*)</Data>' % (name, name))
@@ -53,17 +53,29 @@ def get_data(xml, name):
 
 def import_xml(filename):
 
+	# 4624 - Login 		528
+	# 4647 - Logoff 	551
+
+	#[*] Keys: Category, Description, Data, Domain\User, Date&Time, Source, Computer, Time, Date, Type, Event
+
 	sessions = {}
+	user_sessions = {}
+	count = 0
 
 	with open(filename, 'r') as f:
+		print "[*] Reading EVTX file %s" % filename
 		with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as buf:
 			fh = FileHeader(buf, 0x0)
 			count = 0
 			for xml, record in evtx_file_xml_view(fh):
+				if (count % 2000) == 0:
+					print "%s records read"
+				count +=1
+
 				match = eid_regex.search(xml)
 				eid = int(match.group('eid'))
 				session_id = get_data(xml, 'TargetLogonId')
-
+				
 				# Insert new session in dictionary
 				if sessions.get(session_id, None) == None:
 					sessions[session_id] = {}
@@ -78,11 +90,12 @@ def import_xml(filename):
 					info = {}
 					info['logon_type'] = get_data(xml, 'LogonType')
 					info['eid'] = str(eid)
-					info['username'] = get_data(xml, 'TargetDomainName') + '\\' + get_data(xml, 'TargetUserName')
 					info['ip'] = get_data(xml, 'IpAddress') + ':' + get_data(xml, 'IpPort')
-					info['datetime'] = parse(time_regex.search(xml).group('time'))
+					info['datetime'] = parse(time_regex.search(xml).group('time')[:-7])
 
 					sessions[session_id][str(eid)] = info
+					username = get_data(xml, 'TargetDomainName') + '\\' + get_data(xml, 'TargetUserName')
+					sessions[session_id]['username'] = username
 				
 				elif eid in EVTX_LOGOFF:
 					# Ignore if orphan session
@@ -91,13 +104,9 @@ def import_xml(filename):
 
 					info = {}
 					info['eid'] = str(eid)
-					info['datetime'] = parse(time_regex.search(xml).group('time'))
+					info['datetime'] = parse(time_regex.search(xml).group('time')[:-7])
 					sessions[session_id][str(eid)] = info
-					
-# 4624 - Login 		528
-# 4647 - Logoff 	551
 
-#[*] Keys: Category, Description, Data, Domain\User, Date&Time, Source, Computer, Time, Date, Type, Event
 
 	return sessions
 
@@ -105,7 +114,7 @@ def import_csv(filename, delimiter=';', quotechar='"'):
 
 	with open (filename, 'rU') as csvfile:
 		logs = []
-		print "[*] Reading file %s" % filename
+		print "[*] Reading CSV file %s" % filename
 		logreader = csv.DictReader(csvfile, delimiter=delimiter, quotechar=quotechar)
 
 # Audit Success;23/05/2013;09:00:00;23/05/2013 09:00:00;538;Security;Ouverture/Fermeture de session;\S-1-5-21-2052699199-3915784498-1582209984-43253;USER01;"Fermeture de la session utilisateur : Utilisateur :        username Domaine :        userdomain Id. de la session :        (0x0,0xB38D21AB) Type de session :        4"; 
@@ -119,6 +128,8 @@ def import_csv(filename, delimiter=';', quotechar='"'):
 		# return logs[::-1]
 
 		sessions = {}
+		user_sessions = {}
+
 		count = 0
 
 		for log in logs:
@@ -137,82 +148,99 @@ def import_csv(filename, delimiter=';', quotechar='"'):
 				info = {}
 				info['eid'] = log['Event']
 				info['datetime'] = parse(log['Date&Time'])
-				info['username'] = re.search("%s\W+(?P<username>[\w\.\-$]+)\n" %USERNAME_STRING, log['Description']).group('username')
+				
 				sessions[session_id][log['Event']] = info
+				username = re.search("%s\W+(?P<username>[\w\.\-$]+)\n" %USERNAME_STRING, log['Description']).group('username')
+				sessions[session_id]['username'] = username
 
-			if int(log['Event']) in EVT_LOGOFF:
+			elif int(log['Event']) in EVT_LOGOFF:
 				if sessions.get(session_id, None) == None: # Avoid orphan sessions
 					continue
 				info = {}
 				info['eid'] = log['Event']
 				info['datetime'] = parse(log['Date&Time'])
+
 				sessions[session_id][log['Event']] = info
 
 		print "[*] %s sessions found (%s lines parsed)" % (len(sessions), count)
-
 		return sessions
 
-		# print "[*] Session logs: %s" % len(session_list)
-		# session_list = list(set(session_list)) # uniq
-		# print "[*] Unique sessions: %s" % len(session_list)
+def sessions2timeline(sessions):
 
-		# sessions = []
-		# for s in session_list:
-		# 	sessions.append({'ID': s})
-
-		# users = {}
-
-		# for s in sessions:
-		# 	s['log_entries'] = [l for l in logs if is_session(s['ID'], l)]
+		# generate session list by user
+		user_sessions = {}
+		for sid in [s for s in sessions if sessions[s].get('username', None) != None]:
+			s = sessions[sid]
 			
-		# 	s['dates'] = []
-		# 	s['timestamps'] = []
-		# 	for l in s['log_entries']:
-		# 		s['dates'].append(parse(l['Date&Time']))
-		# 		s['timestamps'].append(calendar.timegm(parse(l['Date&Time']).timetuple()))
-		# 		if l['Event'] in ["540", "538", "528", "551"] :
-		# 			try:
-		# 				s['user'] = re.search("tilisateur\W+([\w\.\-$]+)\n", l['Description']).group(1)
-		# 				#print s['user']
-		# 			except Exception, e:	
-		# 				print "[-] User not found for session"
-		# 				print l
-		# 				exit()
-
-		# 	s['dates'].sort()
-		
-		# 	if users.get(s['user']) == None:
-		# 		users[s['user']] = []
-		# 	users[s['user']].append(s)
-		
-		lanes = [u for u in users]
+			if user_sessions.get(s['username'], None) == None:
+				user_sessions[s['username']] = {}
+			
+			user_sessions[s['username']][sid] = s
+			
+		print "[*] Unique users: %s" % len(user_sessions)
+				
+		lanes = [u for u in user_sessions]
 
 		items = []
-		for i, usr in enumerate(users):
-			for l in get_logons(users[usr]):
-				if l['end'] - l['start'] < datetime.timedelta(seconds=10):
-					l['end'] = l['start'] + datetime.timedelta(seconds=10)
-				items.append({'lane': i, 'id':str(l['start'])[-8:], 'start': str(l['start']), 'end': str(l['end'])})
-			
+		for i, username in enumerate(user_sessions):
 
+			for user_session in get_sessions(user_sessions[username]):
+				if user_session['end'] - user_session['start'] < datetime.timedelta(seconds=10):
+					user_session['end'] = user_session['start'] + datetime.timedelta(seconds=10)
+
+				items.append({'info': user_session['info'],'lane': i, 'start': str(user_session['start']), 'end': str(user_session['end'])})
+			
 		time_begin = min([i['start'] for i in items])
 		time_end = max([i['end'] for i in items])
 
+		return {'time_begin': time_begin, 'time_end': time_end, 'items': items, 'lanes': lanes}
+
+
+
+
+def get_sessions(user_sessions):
+	
+	sessions = []
+
+	for sid in user_sessions:
+		s = user_sessions[sid]
+		start, end = None, None
+		for evt in s:
+			if evt != 'username':
+				if int(evt) in EVT_LOGIN or int(evt) in EVTX_LOGIN: # deal with a login event- look for the smallest date for session start
+					if start == None:
+						start = s[evt]['datetime']
+					elif s[evt]['datetime'] < start:
+						start = s[evt]['datetime']
+				if int(evt) in EVT_LOGOFF or int(evt) in EVTX_LOGOFF: # deal with a logoff event- look for the biggest date for session start
+					if end == None:
+						end = s[evt]['datetime']
+					elif s[evt]['datetime'] > end:
+						end = s[evt]['datetime']
+
+		if end == None:
+			end = start
+		if start == None:
+			start = end
+
+		# remove datetime object, which does not parse well to JS
+		for i in s:
+			try:
+				s[i].pop('datetime')
+			except Exception, e:
+				pass
+		sessions.append({'start': start, 'end': end, 'info': s})
+	
+	return sessions
+	
 
 def print_log(log):
 	for key in log:
-		if key == 'log_entries':
-			for entry in log[key]:
-				for k in entry:
-					print "%s:\n%s\n" % (k, entry[k])	
+		for entry in log[key]:
+			for k in entry:
+				print "%s:\n%s\n" % (k, entry[k])	
 		else:
 			print "%s:\n%s\n" % (key, log[key])
-
-def is_session(session_id, log):
-	return log['Description'].find(session_id) != -1
-
-def get_logons(user_sessions):
-	return [{'start': s['dates'][0], 'end': s['dates'][-1:][0]} for s in user_sessions]
 
 if __name__ == '__main__':
 
@@ -227,23 +255,24 @@ if __name__ == '__main__':
 		Parser.error("You must specify a filename")
 
 	if options.csv:
-		logs = import_csv(options.eventlogfile)
+		sessions = import_csv(options.eventlogfile)
 	elif options.evtx:
-		logs = import_xml(options.eventlogfile)
+		sessions = import_xml(options.eventlogfile)
 	else:
 		Parser.error("You must specify a file format format (csv or xml)")
 
-	
+	timeline = sessions2timeline(sessions)
 
-	print "[*] Collected sessions from %s to %s" % ((time_begin), (time_end))
+	print "[*] Mapped %s sessions from %s to %s" % (len(timeline['items']), timeline['time_begin'], timeline['time_end'])
+
 
 	js = open('timeline/evtdata.js','w+')
 
-	js.write("var lanes = %s,\n" % str(lanes))
+	js.write("var lanes = %s,\n" % str(timeline['lanes']))
 	js.write("laneLength = lanes.length,\n")
-	js.write("items = %s,\n" % items)
-	js.write("timeBegin = \'%s\',\n" % time_begin)
-	js.write("timeEnd = \'%s\';\n" % time_end)
+	js.write("items = %s,\n" % timeline['items'])
+	js.write("timeBegin = \'%s\',\n" % timeline['time_begin'])
+	js.write("timeEnd = \'%s\';\n" % timeline['time_end'])
 
 	js.close()
 
